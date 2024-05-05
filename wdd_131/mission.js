5af99ef407c6d1c880ace22b194550af9a44b6bc
Bryer Johnson <joh21119@byui.edu>
2024-05-04 18:26:07 -0600

    changes 234234234

--- wdd_131/mission.js
+++ wdd_131/mission.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const themeSelect = document.getElementById("theme-select");
     const body = document.body;
+    const logo = document.querySelector('.logo');
 
     function changeTheme() {
         if (themeSelect.value === "dark") {
             body.classList.add("dark");
+            logo.src = "byui-logo_white.png"; // Change logo to white for dark theme
         } else {
             body.classList.remove("dark");
+            logo.src = "byui-logo_blue.png"; // Change logo to blue for light theme
         }
     }
 
