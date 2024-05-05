34c02ed21d9c7b0bca5bf467579e9aa53991375a
Bryer Johnson <joh21119@byui.edu>
2024-05-04 18:11:13 -0600

    changes to java script

--- wdd_131/mission.js
+++ wdd_131/mission.js
@@ -1,18 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
     const themeSelect = document.getElementById("theme-select");
+    const logoContainer = document.querySelector('.logo');
 
     function changeTheme() {
         const body = document.body;
-        const logo = document.querySelector('.logo');
-        
+
         if (themeSelect.value === "dark") {
             body.classList.add("dark");
-            logo.src = "byui-logo_white.png";
+            body.classList.remove("light");
         } else {
+            body.classList.add("light");
             body.classList.remove("dark");
-            logo.src = "byui-logo_blue.png";
+        }
+        updateLogo();
+    }
+
+    function updateLogo() {
+        const body = document.body;
+
+        if (body.classList.contains("dark")) {
+            logoContainer.classList.remove("light");
+            logoContainer.classList.add("dark");
+        } else {
+            logoContainer.classList.remove("dark");
+            logoContainer.classList.add("light");
         }
     }
 
     themeSelect.addEventListener("change", changeTheme);
+
+    // Initial call to set the initial theme and logo
+    changeTheme();
 });
